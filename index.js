0357d1e77b9cea1f3c5fe02d4f0154c59f99f873
ShashankAgrawal003 <agrawalshashank003@gmail.com>
2022-10-22 12:22:29 +0530

    Successfully Data Uploaded

--- index.js
+++ index.js
@@ -1,29 +1,39 @@
-var express= require("express");
-var bodyParser=require("body-parser");
-var mongoose=require("mongoose");
+var express = require("express");
+var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
+const path = require("path");
 
-
-const app=express();
+const app = express();
+const port = process.env.PORT || 3000;
 app.use(bodyParser.json());
-// app.use(express.static('/'));
-app.use(bodyParser.urlencoded({
-    extended:true
-}));
+app.use(express.static("public"));
+app.use(
+  bodyParser.urlencoded({
+    extended: false,
+  })
+);
 
-mongoose.connect('mongodb://localhost:27017/mydb',{
-    useNewUrlParser:true,
-    useUnifiedTopology:true,
+mongoose.connect("mongodb://localhost:27017/mydb", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
-var db=mongoose.connection;
-db.on('error',()=>console.log("Error in Connecting to Database"));
-db.once('open',()=>console.log("Connected to Database"));
+var db = mongoose.connection;
+db.on("error", () => console.log("Error in Connecting to Database"));
+db.once("open", () => console.log("Connected to Database"));
 
-app.post("/submit",(req,res)=>{
+app.get("/", (req, res) => {
+  res.set({ "Allow-access-Allow-Origin": "*" });
+  return res.sendFile(path.join(__dirname + "/public/index.html"));
+
+});
+
+app.post("/submit", async (req, res) => {
+  try {
     var category = req.body.category;
     var model = req.body.model;
     var serialNumber = req.body.serialNumber;
     var date = req.body.date;
-    var file=req.body.file;
+    var file = req.body.file;
 
     console.log(category);
     console.log(model);
@@ -31,29 +41,27 @@ app.post("/submit",(req,res)=>{
     console.log(date);
     console.log(file);
 
-
     var data = {
-        "category": category,
-        "model" : model,
-        "serialNumber": serialNumber,
-        "date" : date,
-        "file":file
-    }
-// yaha _ lagaya hai extra
-    db.collection('values').insertOne(data,(err,_collection)=>{
-        if(err){
-            throw err;
-        }
-        console.log("Record Inserted Successfully");
+      category: category,
+      model: model,
+      serialNumber: serialNumber,
+      date: date,
+      file: file,
+    };
+
+    db.collection("values").insertOne(data, (err, collection) => {
+      if (err) {
+        throw err;
+      }
+      console.log("Record Inserted Successfully");
     });
 
-    return res.redirect('result.html');  
-
+    return res.redirect("result.html");
+  } catch (error) {
+    res.status(400).send(error);
+  }
 });
 
-app.get('/',(req,res)=>{
-    res.set({"Allow-access-Allow-Origin":'*'});
-    // ye return line kyu hai
-    return res.redirect('index.html');
-}).listen(3000);
-console.log("Listening to the port 3000");
+app.listen(port, () => {
+  console.log(`Listening to the port no: ${port}`);
+});
